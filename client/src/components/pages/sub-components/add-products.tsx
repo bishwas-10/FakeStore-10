c77ae810ae6dd47e7c23791aa95e70bb0ecd8a5b
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-02-02 19:58:39 +0545

    done hai gaaro vaye ni

--- client/src/components/pages/sub-components/add-products.tsx
+++ client/src/components/pages/sub-components/add-products.tsx
@@ -20,26 +20,27 @@ export const productSchema = z.object({
   price: z.string().min(1, "title is required"),
   category: z.string().min(1, "title is required"),
   description: z.string().min(1, "title is required"),
-  image: z
-    .any()
-    .refine(
-      (files) => ACCEPTED_IMAGE_MIME_TYPES.includes(files?.type),
-      "Only .jpg, .jpeg, .png and .webp formats are supported."
-    ),
+  image: z.any().refine(
+    (base64Data) => {
+      // Check if the base64 data starts with 'data:image'
+      return base64Data && base64Data.startsWith("data:image");
+    },
+    "Only image files in base64 format are supported."
+  ),
 });
 
 export type TProductSchema = z.infer<typeof productSchema>;
 
 const AddProducts = () => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-
+  const [selectedFile, setSelectedFile] = useState<string>();
+  const [imageName, setImageName] = useState<string>("");
   const dispatch = useDispatch();
 
   const product = useSelector((state: RootState) => state.product);
-  console.log(product)
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
     setValue,
   } = useForm<TProductSchema>({
@@ -50,10 +51,20 @@ const AddProducts = () => {
     reValidateMode: "onChange",
   });
 
+  const convert2base64 = (file: any) => {
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImageName(file.name);
+      setSelectedFile(reader.result?.toString());
+      setValue("image", reader.result?.toString() as string);
+    };
+    reader.readAsDataURL(file);
+  };
+
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
-    setSelectedFile(file);
-    setValue("image", file);
+    convert2base64(file);
   };
   const onSubmit = async (data: TProductSchema) => {
     dispatch(addProduct(data));
@@ -112,32 +123,29 @@ const AddProducts = () => {
             </div>
             <div className="flex flex-col gap-1">
               <div>
-                <Button variant="outlined">
-                  <input
-                    type="file"
-                    className="hidden"
-                    id="fileInput"
-                    onChange={handleFileChange}
-                  />
-                  <FormLabel
-                    htmlFor="fileInput"
-                    className="flex flex-row gap-2 items-center"
-                  >
-                    <UploadIcon />
-                    <span className="whitespace-nowrap text-sm">
-                      choose your image
-                    </span>
-                  </FormLabel>
-                </Button>
-                {selectedFile && (
-                  <div className="flex items-center gap-2">
-                    <span>Selected file: {selectedFile.name}</span>
-                    {/* You can add a button to remove the selected file if needed */}
-                  </div>
+                {!watch("image") || watch("image").length === 0 ? (
+                  <Button variant="outlined">
+                    <input
+                      type="file"
+                      className="hidden"
+                      id="fileInput"
+                      onChange={handleFileChange}
+                    />
+                    <FormLabel
+                      htmlFor="fileInput"
+                      className="flex flex-row gap-2 items-center"
+                    >
+                      <UploadIcon />
+                      <span className="whitespace-nowrap text-sm">
+                        choose your image
+                      </span>
+                    </FormLabel>
+                  </Button>
+                ) : (
+                  <strong>Selected file: {imageName}</strong>
                 )}
               </div>
               <>
-                {" "}
                 {errors.image && (
                   <span className="text-red-500">{`${errors.image.message}`}</span>
                 )}
@@ -158,14 +166,16 @@ const AddProducts = () => {
             <div className="w-full h-44 relative bg-white rounded-t-md p-4">
               <div className="absolute z-10 top-2 right-2 flex flex-row items-center justify-center h-6 py-3 px-2 bg-red-500 rounded-full">
                 <p className="text-sm font-semibold text-white">
-                  $ {product.price}
+                  $ {product?.price}
                 </p>
               </div>
+              <div className="">
               <img
-                src={product.image.name}
+                src={product.image}
                 alt={product.title}
-                className="p-4 z-0 object-contain"
+                className="p-4 w-full h-40 object-fit"
               />
+              </div>
             </div>
             <div className="w-full h-32 rounded-b-md flex flex-col border-t border-gray-600 bg-gray-100">
               <div className="flex flex-row w-full pt-1 px-2">
