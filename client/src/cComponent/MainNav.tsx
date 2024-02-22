6178f318537b6a0de2c5f9393f41d1b935b26ebe
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-02-22 20:00:24 +0545

    ho ma

--- client/src/cComponent/MainNav.tsx
+++ client/src/cComponent/MainNav.tsx
@@ -31,13 +31,13 @@ import Drawer from "@mui/material/Drawer";
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
 
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  // borderRadius: theme.shape.borderRadius,
+  // backgroundColor: alpha(theme.palette.common.white, 0.15),
 
   marginLeft: 0,
   width: "100%",
   [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
+    // marginLeft: theme.spacing(1),
     width: "auto",
   },
 }));
