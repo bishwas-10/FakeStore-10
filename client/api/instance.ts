6a56041dc490614a297a7d0fd8489c63c0890e74
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-02-23 20:27:56 +0545

    pleaseeee

--- client/api/instance.ts
+++ client/api/instance.ts
@@ -1,18 +1,17 @@
 import axios from "axios";
 
-const BASE_URL="https://eccomerce-admin1.onrender.com"
+
 
 export const instance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
-    headers:{
-      'Content-Type':'application/json',
-
+  baseURL: "https://eccomerce-admin1.onrender.com",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
   },
-  });
+});
 
-  export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+export const axiosPrivate = axios.create({
+  baseURL: "https://eccomerce-admin1.onrender.com",
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
 });
