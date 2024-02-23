ee07898536ed88287812eeeb213a8591c4ce218c
bishwas-10 <120932783+bishwas-10@users.noreply.github.com>
2024-02-23 10:03:32 +0545

    huna

--- server/server.ts
+++ server/server.ts
@@ -54,7 +54,7 @@ console.log("aayo")
 app.use(express.static(path.resolve(__dirname, 'dist')));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173","*"],
+    origin: ["http://localhost:3000", "http://localhost:5173","https://eccomerce-admin1.vercel.app"],
     methods: "GET,POST, PUT, DELETE, PATCH",
     credentials: true,
     exposedHeaders: ["Access-Control-Allow-Origin"],
